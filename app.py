7c9af2567b6ed4a303a15acfec87e697abdf121e
dktest2 <120356313+dktest2@users.noreply.github.com>
2022-12-13 17:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'GrryMatters'
+   return 'Hello, World!'
 
-
-if __name__ == "__main__":
-    app.run()
+#Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
+os.system("git clone https://dktest2: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@github.com/dktest2/Mdisk-Link-Search-Bot-Pro okk && cd okk && pip3 install -U -r requirements.txt && python3 bot.py &")
