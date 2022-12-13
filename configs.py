ccbd2749e371559c898cc73330bf6e2d6aaf4425
dktest2 <120356313+dktest2@users.noreply.github.com>
2022-12-13 10:16:13 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -7,16 +7,16 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.getenv("API_ID", "9579926"))
+    API_ID = int(os.getenv("API_ID", 9579926))
     API_HASH = os.getenv("API_HASH", "0057feec53650ed8f726c70ceef55ace")
     BOT_TOKEN = os.getenv("BOT_TOKEN", "5504167656:AAETmAT_obKhhB8oACEW12WCglwbuDK3UEI")
     BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "MdiskSearchRobot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOL4Bu7694CJAzh9yF7ewX1SPHxs9hYJdPduQ2k4QyjZU5BE0idYcrrbS9wLKjCCOKAzd8EindP9whMLovN9S91fWp0EC8UdYFghU8zHqyCxF4An3fcmHR5iPtImEnHCWzZYdtASC-QMEZfAyBkAnv04RLhiKLxzp2ya2w6tT5MpO2BTAy6SyJ-L5DIfKu_N5MO6F9_aYPOEvMlkF2UG8As7KY_78pGlTUtRlXMBuftA_uoxdJYkOjAfHg5sa5yZTS37Op9KB4lVRMs-hxn_4tu4TTUEMSZcGWgLoFHakdyz2YuZDTVnibifN010jBm6zUXKBcf3t3nsg6I-rO8Wu_WWM7I4=")
-    CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1001669142815"))
+    CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001669142815))
     BOT_USERNAME = os.getenv("BOT_USERNAME", "DK_Movie_Search_Filter_bot")
-    BOT_OWNER = int(os.getenv("BOT_OWNER", "1898724016"))
+    BOT_OWNER = int(os.getenv("BOT_OWNER", 1898724016))
 #    OWNER_USERNAME = os.getenv("OWNER_USERNAME", "R_KOHLI")
-    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", "-1001741172138")
+    BACKUP_CHANNEL = os.getenv("BACKUP_CHANNEL", -1001741172138)
 #    GROUP_USERNAME = os.getenv("GROUP_USERNAME")
     START_MSG = os.getenv("START_MSG", '''Hᴇʏ Bᴜᴅᴅʏ! 😃
 
@@ -32,10 +32,10 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
     UPDATES_CHANNEL = os.getenv("UPDATES_CHANNEL", None)
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://aaaa:aaaa@cluster0.0uzkggq.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001536727311"))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", -1001536727311))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
-    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", "-1001552154456")
+    UPDATES_CHANNEL_USERNAME = os.getenv("UPDATES_CHANNEL_USERNAME", -1001552154456)
     FORCE_SUB = os.getenv("FORCE_SUB", "False")
     AUTO_DELETE_TIME = int(os.getenv("AUTO_DELETE_TIME", 300))
     MDISK_API = os.getenv("MDISK_API", "SDGrUDqNRFvoT65SJRAh")
