87012076b24401c0e8448cf7cd2b70c4d720f81f
dktest2 <120356313+dktest2@users.noreply.github.com>
2022-12-22 09:52:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/dktest2/MessageSearchBot.git /MdiskWalaBot
-cd /MdiskWalaBot
+git clone https://github.com/dktest2/Mdisk-Search-Bot-Pro.git /MdiskSearchBotV2
+cd /MdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
